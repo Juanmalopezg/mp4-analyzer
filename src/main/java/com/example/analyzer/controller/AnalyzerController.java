9e877ee2388d03bde3f85c72f8f20a8f83aadf07
Juanma López G <juanmanuel.lpzg@gmail.com>
2023-04-03 10:10:46 -0300

    Add non-blocking i/o operations

--- src/main/java/com/example/analyzer/controller/AnalyzerController.java
+++ src/main/java/com/example/analyzer/controller/AnalyzerController.java
@@ -1,6 +1,7 @@
 package com.example.analyzer.controller;
 
 import com.example.analyzer.model.Box;
+import com.example.analyzer.service.BoxService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
@@ -8,32 +9,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
-import java.io.BufferedInputStream;
-import java.io.InputStream;
-import java.net.URI;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class AnalyzerController {
     public static final String DEFAULT_URL = "https://demo.castlabs.com/tmp/text0.mp4";
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final BoxService boxService;
+
+    public AnalyzerController(BoxService boxService) {
+        this.boxService = boxService;
+    }
 
     @GetMapping("/analyze")
     public Mono<ResponseEntity<String>> analyze(@RequestParam(value = "url", defaultValue = DEFAULT_URL) String url) {
-        return Mono.fromCallable(() -> {
-                    try (InputStream inputStream = new URI(url).toURL().openStream()) {
-                        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-                        return bufferedInputStream.readAllBytes();
-                    }
-                })
-                .map(bytes -> {
-                    ByteBuffer byteBuffer = ByteBuffer.wrap(bytes).order(ByteOrder.BIG_ENDIAN);
-                    List<Box> boxes = parseBoxes(byteBuffer, 0, bytes.length);
-                    String json = null;
+        HttpClient client = HttpClient.create();
+        return client.get()
+                .uri(url)
+                .responseContent()
+                .aggregate()
+                .asByteArray()
+                .map(s -> {
+                    ByteBuffer byteBuffer = ByteBuffer.wrap(s);
+                    List<Box> boxes = boxService.analyze(byteBuffer, 0, s.length);
+                    String json;
                     try {
                         json = objectMapper.writeValueAsString(boxes);
                     } catch (JsonProcessingException e) {
@@ -45,22 +47,5 @@ public class AnalyzerController {
                         .header("Content-Type", "application/json")
                         .body(json))
                 .onErrorResume(error -> Mono.just(ResponseEntity.badRequest().body(error.getMessage())));
-    }
-
-    private List<Box> parseBoxes(ByteBuffer byteBuffer, int offset, int length) {
-        List<Box> boxes = new ArrayList<>();
-        int end = offset + length;
-        while (offset < end) {
-            int boxSize = byteBuffer.getInt(offset);
-            String boxType = new String(byteBuffer.array(), offset + 4, 4);
-            Box box = new Box(boxSize, boxType);
-            if (boxType.equals("moof") || boxType.equals("traf")) {
-                List<Box> subBoxes = parseBoxes(byteBuffer, offset + 8, boxSize - 8);
-                subBoxes.forEach(box::addSubBox);
-            }
-            boxes.add(box);
-            offset += boxSize;
-        }
-        return boxes;
     }
 }
